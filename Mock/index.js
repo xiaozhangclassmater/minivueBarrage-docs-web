985097788c1520e1527f5bb5a70794b087df9cde
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-07-29 11:49:12 +0800

    feat:Test the minivueBarrage plugin to see if it works properly

--- Mock/index.js
+++ Mock/index.js
@@ -1,10 +1,11 @@
 const Mock = require('mockjs');
 const data = Mock.mock({
-  'list|1-100' : [{
-    id : '@id',
-    type : "common",
-    content : '@name',
-    url : Mock.Random.url(),
+  'list|1-100': [{
+    id: '@id',
+    type: "common",
+    content: '@name',
+    // url: 'https://www.baidu.com',
+    imgLink: Mock.Random.image()
   }]
 })
 
