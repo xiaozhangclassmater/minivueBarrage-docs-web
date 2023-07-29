2327cd6580b7b0850a8a6c39ec46e287d94f4f76
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-07-29 22:01:03 +0800

    feat:Test the minivuebarrage pack

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import i18n from '@/lang'
 import '@/styles/index.scss'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
 import 'normalize.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// Vue.use(miniVueBarrage)
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 new Vue({
